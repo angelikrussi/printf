1a57da951b8d9c5405d44d71a5ff2b349f1211be
angelikrussi <hannamrrussi@gmail.com>
2021-07-27 21:11:21 -0500

    Function ctype

--- _ctype.c
+++ _ctype.c
@@ -12,10 +12,10 @@ int (*_ctype(const char *argv, int argc))(va_list)
 	s_printer printers[] = {
 		/* first column ("") is the format and the second*/
 		/* is the list of functions */
-		{"c", p_char},
-		{"s", p_string},
-		{"d", p_int},
-		{"i", p_int},
+		{"c", pr_char},
+		{"s", pr_string},
+		{"d", pr_int},
+		{"i", pr_int},
 		{NULL, NULL},
 	};
 
@@ -24,16 +24,16 @@ int (*_ctype(const char *argv, int argc))(va_list)
 	/* for i that starts at 0; until list in position i is */
 	/* part of args and be other than null; i increases one by one */
 
-	for (k = 0; printer[k].arg != NULL; k++)
+	for (k = 0; printers[k].arg != NULL; k++)
 	{
 		/* for i that starts at 0; until list in position i is*/
 		/* part of args and be other than null; i increases one by one*/
 
-		if (printer[k].arg[0] == argv[argc])
+		if (printers[k].arg[0] == argv[argc])
 		{
 			/* returns list in position i that is part of func */
 			/*  which is the list of  functions */
-			return (printer[k].f);
+			return (printers[k].f);
 		}
 	}
 	/* if it does not meet the previous conditions, it returns NULL */
